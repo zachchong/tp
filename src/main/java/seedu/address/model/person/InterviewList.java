c50a902f749d9c04070a93b09d84d2c0d14cacfb
JBYoonn <e1398360@u.nus.edu>
2025-10-09 14:46:17 +0800

    Update InterviewList.java
    Add basic methods to InterviewList.java

--- src/main/java/seedu/address/model/person/InterviewList.java
+++ src/main/java/seedu/address/model/person/InterviewList.java
@@ -1,4 +1,72 @@
 package seedu.address.model.person;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a Person's list of interviews in the address book.
+ */
 public class InterviewList {
+
+    private List<String> interviewList;
+
+    /**
+     * Constructs a {@code InterviewList}.
+     *
+     * @param interviewList List of initial interviews.
+     */
+    public InterviewList(List<String> interviewList) {
+        if (interviewList != null) {
+            interviewList = interviewList;
+        } else {
+            interviewList = new ArrayList<>();
+        }
+    }
+
+    /**
+     * Returns the interview list.
+     */
+    public List<String> getInterviewList() {
+        return interviewList;
+    }
+
+    /**
+     * Adds an interview to the interview list.
+     */
+    public void add(String interview) {
+        interviewList.add(interview);
+    }
+
+    /**
+     * Removes an interview to the interview list.
+     */
+    public void remove(String interview) {
+        interviewList.remove(interview);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (other == this) {
+            return true;
+        }
+
+        // instanceof handles nulls
+        if (!(other instanceof InterviewList)) {
+            return false;
+        }
+
+        InterviewList otherList = (InterviewList) other;
+        return interviewList.equals(otherList.interviewList);
+    }
+
+    @Override
+    public int hashCode() {
+        return interviewList.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return interviewList.toString();
+    }
+
 }
