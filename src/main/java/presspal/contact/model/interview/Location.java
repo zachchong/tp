5d3a96e46567963a7784d1643bd0ce4e6474c968
Jessie <jcc537743@gmail.com>
2025-10-09 16:39:42 +0800

    Update Location to include avadoc comments

---
+++ src/main/java/presspal/contact/model/interview/Location.java
@@ -0,0 +1,26 @@
+package presspal.contact.model.interview;
+
+import presspal.contact.model.AddressBook;
+
+/**
+ * Represents the location of interview.
+ */
+public class Location {
+    /** The textual representation of the interview location. */
+    private String location;
+
+    /** Constructs a Location object */
+    public Location(String location){
+        this.location = location;
+    }
+
+    /** Returns the interview location as a String */
+    public String getLocation() {
+        return this.location;
+    }
+
+    /** Updates the interview location to a new specified value */
+    public void setLocation(String newLocation) {
+        this.location = newLocation;
+    }
+}
