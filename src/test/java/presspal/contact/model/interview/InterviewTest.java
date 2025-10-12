73f3bdb1b5669fb760e120cfee8b21027c167681
Jessie <jcc537743@gmail.com>
2025-10-12 12:38:59 +0800

    Edit InterviewTest.java

--- src/test/java/presspal/contact/model/interview/InterviewTest.java
+++ src/test/java/presspal/contact/model/interview/InterviewTest.java
@@ -2,9 +2,9 @@ package presspal.contact.model.interview;
 
 import java.time.LocalDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
