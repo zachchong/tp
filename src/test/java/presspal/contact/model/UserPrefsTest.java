af7c97417986264151eaafec01aa341a1fa8b888
Lucasn24 <e1406319@u.nus.edu>
2025-10-14 14:47:59 +0800

    Add testcases

--- src/test/java/presspal/contact/model/UserPrefsTest.java
+++ src/test/java/presspal/contact/model/UserPrefsTest.java
@@ -1,11 +1,16 @@
 package presspal.contact.model;
 
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static presspal.contact.testutil.Assert.assertThrows;
 
-import java.util.Objects;
+import java.nio.file.Paths;
 
 import org.junit.jupiter.api.Test;
+
+import presspal.contact.commons.core.GuiSettings;
+
 
 public class UserPrefsTest {
 
@@ -22,12 +27,21 @@ public class UserPrefsTest {
     }
 
     @Test
-    public void testHashCode() {
-        UserPrefs userPrefs = new UserPrefs();
-        userPrefs.setContactBookFilePath(null);
-        userPrefs.setGuiSettings(null);
+    public void hashCode_equalObjects_sameHashCode() {
+        UserPrefs userPrefs1 = new UserPrefs();
+        UserPrefs userPrefs2 = new UserPrefs();
 
-        assertEquals(userPrefs.hashCode(),
-                Objects.hash(userPrefs.getContactBookFilePath(), userPrefs.getGuiSettings()));
+        // Same default values should have same hashcode
+        assertEquals(userPrefs1.hashCode(), userPrefs2.hashCode());
+
+        // Modified prefs with same values should have same hashcode
+        GuiSettings newSettings = new GuiSettings(800, 600, 0, 0);
+        userPrefs1.setGuiSettings(newSettings);
+        userPrefs2.setGuiSettings(newSettings);
+        assertEquals(userPrefs1.hashCode(), userPrefs2.hashCode());
+
+        // Different paths should result in different hashcodes
+        userPrefs1.setContactBookFilePath(Paths.get("data", "different.json"));
+        assertNotEquals(userPrefs1.hashCode(), userPrefs2.hashCode());
     }
 }
