72d8f674f5a45779168bb711d72f285a34096e23
Jessie <jcc537743@gmail.com>
2025-10-15 01:14:01 +0800

    Update reviewed comments

--- src/test/java/presspal/contact/model/interview/HeaderTest.java
+++ src/test/java/presspal/contact/model/interview/HeaderTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
