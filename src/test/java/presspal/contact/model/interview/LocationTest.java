1e4a55f420d18fc0300b78b1de63ddd750ace279
Jessie <jcc537743@gmail.com>
2025-10-15 11:37:17 +0800

    Fix checkstyles

--- src/test/java/presspal/contact/model/interview/LocationTest.java
+++ src/test/java/presspal/contact/model/interview/LocationTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
