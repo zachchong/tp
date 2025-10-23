8715706c12a620c27d6d296efa4bb23bab2b6c0c
KieronOei <65146443+KieronOei@users.noreply.github.com>
2025-10-23 17:55:31 +0800

    Fix UI bug: Add message to indicate no interviews
    * Update text to be displayed if contact has no interviews

--- src/main/java/presspal/contact/ui/PersonCard.java
+++ src/main/java/presspal/contact/ui/PersonCard.java
@@ -69,7 +69,10 @@ public class PersonCard extends UiPart<Region> {
         person.getCategories().stream()
                 .sorted(Comparator.comparing(category -> category.categoryName))
                 .forEach(category -> categories.getChildren().add(new Label(category.categoryName)));
-        interview.setText(person.getNextUpcomingInterview().map(Interview::getDisplayString).orElse(""));
+        interview.setText(person
+                .getNextUpcomingInterview()
+                .map(Interview::getDisplayString)
+                .orElse("No upcoming interviews"));
     }
     private void hideLabel(Label label) {
         label.setText("");
