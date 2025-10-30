ba72be7510c3b542a842e6877dc259c389a95368
Jessie <jcc537743@gmail.com>
2025-10-30 23:27:04 +0800

    Update DeleteCommandParserTest

--- src/test/java/presspal/contact/logic/parser/EditCommandParserTest.java
+++ src/test/java/presspal/contact/logic/parser/EditCommandParserTest.java
@@ -78,9 +78,6 @@ public class EditCommandParserTest {
 
         // invalid arguments being parsed as preamble
         assertParseFailure(parser, "1 some random string", MESSAGE_INVALID_FORMAT);
-
-        // invalid prefix being parsed as preamble
-        assertParseFailure(parser, "1 i/ string", MESSAGE_INVALID_FORMAT);
     }
 
     @Test
